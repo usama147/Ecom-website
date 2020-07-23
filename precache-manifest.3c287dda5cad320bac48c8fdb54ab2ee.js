768edd05e1c38c917060daae0bcdd833e6d98af2
usama147 <gierdienusama@gmail.com>
2020-07-24 00:19:33 +0200

    Updates

---
+++ precache-manifest.3c287dda5cad320bac48c8fdb54ab2ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36726dd88f6493a1e6c4e9dcdff4080",
+    "url": "/Ecom-website/index.html"
+  },
+  {
+    "revision": "9e42bb6cbe087e34a74b",
+    "url": "/Ecom-website/static/css/main.2a6bef13.chunk.css"
+  },
+  {
+    "revision": "1670eecf19881bc48a4c",
+    "url": "/Ecom-website/static/js/2.6c6ac6ca.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/Ecom-website/static/js/2.6c6ac6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e42bb6cbe087e34a74b",
+    "url": "/Ecom-website/static/js/main.28939546.chunk.js"
+  },
+  {
+    "revision": "b29b86fc23bfa02c38fa",
+    "url": "/Ecom-website/static/js/runtime-main.c1f58cd8.js"
+  },
+  {
+    "revision": "64dca5aecc3a24644f4e9c9a0e9b156a",
+    "url": "/Ecom-website/static/media/download.64dca5ae.png"
+  },
+  {
+    "revision": "c8112d8c3b9911bc3dd8a842afded266",
+    "url": "/Ecom-website/static/media/logo.c8112d8c.png"
+  },
+  {
+    "revision": "1cfa729dd2c31e9fe3f83df40a01fce5",
+    "url": "/Ecom-website/static/media/pic.1cfa729d.jpg"
+  }
+]);
